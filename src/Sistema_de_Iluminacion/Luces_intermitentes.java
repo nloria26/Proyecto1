865ca57aedaa3f9f41d0c36ea6db569fc8b6eca3
nloria26 <niloriaug@est.utn.cr>
2025-07-26 17:00:19 -0600

    Clases del Sistema_de_Iluminacion
    Se creo la clases, luces_delanteras, luces_intermitentes, lusces_emergencia y luces_puertas. Tods las clase en el pequete Sistems de iluminacion implementando la calase abstracta Metodo_de_Encendido

---
+++ src/Sistema_de_Iluminacion/Luces_intermitentes.java
@@ -0,0 +1,23 @@
+
+package Sistema_de_Iluminacion;
+
+import Metodo_de_Encendido.Metodo_de_Encendido;
+
+public class Luces_intermitentes extends Metodo_de_Encendido{
+
+    @Override
+    public void apagado() {
+        super.apagado(); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
+    }
+
+    @Override
+    public void encender() {
+        super.encender(); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
+    }
+
+    @Override
+    public boolean isEncendido() {
+        return super.isEncendido(); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
+    }
+    
+}
