fc2fe472bd73dd0b63d5fb6e23fe8e7bd9df9866
SoapGiutroll <Giutroll117troll@gmail.com>
2025-07-30 17:48:13 -0600

    Se crea el paquete y sistema de frenos
    Se creoa el paquete y sistema de frenos encargado de mandar una alarma si el carro intenta avanzar si tiene el freno activado o tiene un obstaculo detras

---
+++ src/SistemaFrenos/Sistemafrenos.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package SistemaFrenos;
+
+/**
+ *
+ * @author UTN-GIU
+ */
+public class Sistemafrenos {
+ private boolean frenoMano;     
+    private boolean obstaculoAtras; 
+    private boolean alarmaActiva;
+    private String Razon;
+
+
+    public Sistemafrenos() {
+        this.frenoMano = true; 
+        this.obstaculoAtras = false;
+        this.alarmaActiva = false;
+    }
+
+    public void activarFrenoMano() {
+        this.frenoMano = true;
+    }
+
+    public void quitarFrenoMano() {
+        this.frenoMano = false;
+    }
+
+
+    public void verificarMovimiento(boolean enReversa) {
+        this.alarmaActiva = false;
+
+        if (frenoMano) {
+            this.alarmaActiva = true;
+            this.Razon="Hey, cuidado, romperas el motor, primero quita el freno de mano para avanzar";
+            return;
+        }
+
+        if (enReversa && obstaculoAtras) {
+            this.alarmaActiva = true;
+            this.Razon="ATENCION, OBSTACULO DETRAS DETECTADO";
+            return;
+        }
+    }
+
+    public boolean isAlarmaActiva() {
+        return alarmaActiva;
+    }
+
+}
+
